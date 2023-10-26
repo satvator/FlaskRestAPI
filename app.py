99f0d4d654db18e772d2a4196f232351ce6faaf0
satvator <sattamshukla2002@gmail.com>
2023-10-26 19:06:06 +0530

    add JWT authentication

--- app.py
+++ app.py
@@ -1,9 +1,64 @@
 from flask import Flask, request
 from flask_restful import Api, Resource
-
+from flask_sqlalchemy import SQLAlchemy
+from flask_jwt_extended import create_access_token, JWTManager, get_jwt_identity, jwt_required
 app=Flask(__name__)
-api=Api(app)
 
+
+app.config['SECRET_KEY']='dlgnsg'
+app.config['SQLALCHEMY_DATABASE_URI']='sqlite:///database.db'
+db=SQLAlchemy(app)
+api=Api(app)
+jwt=JWTManager(app)
+
+class User(db.Model):
+    id=db.Column(db.Integer, primary_key=True)
+    username=db.Column(db.String(100), unique=True, nullable=False)
+    password=db.Column(db.String(100), nullable=False)
+    
+with app.app_context():
+    db.create_all()
+
+class UserRegistration(Resource):
+    def post(self):
+        data=request.get_json()
+        username=data['username']
+        password=data['password']
+        
+        if not username or not password:
+            return {'message': 'Missing username or password'}, 400
+            
+        if User.query.filter_by(username=username).first():
+            return {'message': "username already taken!!"}, 400
+
+        new_user=User(username=username, password=password)
+        db.session.add(new_user)
+        db.session.commit()
+        return {'message': 'User created successfully!!'}, 200
+    
+class UserLogin(Resource):
+    def post(self):
+        data=request.get_json()
+        username=data['username']
+        password=data['password']
+        user=User.query.filter_by(username=username).first()
+        if user and user.password==password:
+            access_token=create_access_token(identity=user.id)
+            return {'access_token': access_token }, 200
+        return {'message': 'Invalid credentials!!'}, 401
+    
+class ProtectedResource(Resource):
+    @jwt_required()
+    def get(self):
+        current_user_id = get_jwt_identity()
+        return {'message': f"hello user {current_user_id}, you accessed the protected resource"}, 200
+
+api.add_resource(UserRegistration, '/register')
+api.add_resource(UserLogin, '/login')
+api.add_resource(ProtectedResource,'/secure')
+    
+    
+    
 data= [
     {"id": 1, 'name': "user1", 'age': 49},
     {"id": 2, 'name': "user2", 'age': 69}
